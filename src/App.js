f880a6a06bcefcd5acc1b95243cfab7487e2caef
AlbertSimonA <106898361+AlbertSimonA@users.noreply.github.com>
2023-02-22 10:52:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "./Components/AppLayout";
+import RestaurantMenu from "./Components/RestaurantMenu";
+
+const App = () => {
+  return (
+    <div>
+      <AppLayout />
+    </div>
+  );
+};
+export default App;
